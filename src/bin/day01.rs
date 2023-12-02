1a8f2fe9c3b989164985573da304d0fa0fd6e167
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2023-12-02 16:27:59 +0200

    feat: day01 part 2 solution + formatting

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,22 +1,43 @@
 fn main() {
     // read input from the input dir/file
     let input = std::fs::read_to_string("input/day01").unwrap(); 
-    // call the function for every line
-    let sum1: usize = input.lines().map(part1_calibration_value).sum(); 
 
-    println!("Part1: {sum1}");
+    // call the functions for every line
+    let sum1: usize = input.lines().map(part1_calibration_value).sum(); 
+    let sum2: usize = input.lines().map(part2_calibration_value).sum(); 
+
+    println!("Part1 answer: {sum1}");
+    println!("Part2 answer: {sum2}");
 }
 
 fn part1_calibration_value(line: &str) -> usize {
     let first_char = line
         .chars()
-        .find(|c| c.is_numeric())
+        .find(|char| char.is_numeric())
         .unwrap();
+
     let last_char = line
         .chars()
-        .rev()
-        .find(|c| c.is_numeric())
+        .rev() // reverse to get last char first
+        .find(|char| char.is_numeric())
         .unwrap();
 
     format!("{first_char}{last_char}").parse().unwrap()
+}
+
+fn part2_calibration_value(line: &str) -> usize {
+    let line = line
+        .replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e"); 
+    // fix cases where string/number intertwine
+    // normalize the input
+
+    part1_calibration_value(&line) // call function from part1
 }
