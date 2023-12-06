266e04c6bb0b14a7d432ad1b72919e05010ac81c
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2023-12-06 19:49:51 +0200

    feat: day04 part 2

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -6,10 +6,13 @@ fn main() {
     let get_matching_cards: Vec<usize> = input.lines().map(get_matching_cards).collect(); 
     // call get_matching_cards() for every line of input, collect in a list/Vec
 
-    let sum: usize = get_matching_cards.into_iter().map(calculate_card_score).sum();
+    let sum: usize = get_matching_cards.clone().into_iter().map(calculate_card_score).sum();
     // call calculate_card_score for each element, sum values
+    let sum2: usize = calculate_card_count(&get_matching_cards);
 
-    println!("Part1 answer: {sum}");
+    println!("Part 1 answer: {sum}");
+    println!("Part 2 answer: {sum2}");
+
 }
 
 fn get_matching_cards(line: &str) -> usize {
@@ -31,4 +34,19 @@ fn calculate_card_score(matches: usize) -> usize {
     } else {
         0
     }
+}
+
+fn calculate_card_count(matching_cards: &Vec<usize>) -> usize {
+    let mut card_counts = vec![1; matching_cards.len()];
+    for (i, matches) in matching_cards.iter().enumerate() {
+        for j in i + 1..=i + matches {
+            if card_counts.get(j).is_none() {
+                break;
+            }
+            card_counts[j] += card_counts[i];
+        }
+    }
+
+    let p2: usize = card_counts.iter().sum();
+    p2
 }
